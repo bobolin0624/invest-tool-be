c84640103824ebf103988bca8ca1c032f0e6fa86
bobo.lin <bobolincowo@gmail.com>
2025-06-19 17:09:52 +0800

    feat: add post / patch investment api

--- src/routes/investments.ts
+++ src/routes/investments.ts
@@ -10,10 +10,34 @@ router.get('/investments', async (ctx) => {
     id: doc.id,
     ...doc.data(),
   }));
-  ctx.body = { 
+  ctx.body = {
     status: 'ok',
     data: investments
   };
+})
+
+router.post('/investments', async (ctx) => {
+  console.log('create investments');
+  // fields: userId, name, cost, type, value, shares
+  const createData = ctx.request.body as Object;
+  const newInvestment = {
+    ...createData,
+    createdAt: new Date().toISOString(),
+  }
+  const createdResult = await db.collection('investments').add(newInvestment);
+  ctx.body = {
+    status: 'ok',
+    data: {
+      id: createdResult.id,
+      ...newInvestment,
+    }
+  }
+})
+
+router.patch('/investments/:id', async (ctx) => {
+  console.log('upload investments');
+  const investmentId = ctx.params.id
+  db.collection("users").doc(investmentId).update({foo: "bar"});
 })
 
 export default router;
