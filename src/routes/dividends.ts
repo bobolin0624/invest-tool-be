fb9cb1bbd7ee581b523f2cd935813f12520fa5d2
bobo.lin <bobolincowo@gmail.com>
2025-06-24 10:38:44 +0800

    feat: dividend patch delete api

--- src/routes/dividends.ts
+++ src/routes/dividends.ts
@@ -30,4 +30,64 @@ router.get('/investments/:id/dividends',
   }
 )
 
+router.post('/investments/:id/dividends',
+  async (ctx) => {
+    try {
+      console.log('create dividends');
+      // fields: investmentId, value, date
+      const investmentId = ctx.params.id;
+      const createData = ctx.request.body as Object;
+      const newDividend = {
+        ...createData,
+        investmentId,
+        createdAt: new Date().toISOString(),
+      }
+      const createdResult = await db.collection('dividends').add(newDividend);
+      ctx.body = {
+        status: 'ok',
+        data: {
+          id: createdResult.id,
+          ...newDividend,
+        }
+      }
+    } catch (error: any) {
+      console.error(error.stack);
+      ctx.throw(500, 'Failed to create dividend');
+    }
+  }
+)
+
+router.patch('/dividends/:id', async (ctx) => {
+  try {
+    console.log('upload dividends');
+    const dividendsId = ctx.params.id;
+    const updateData = ctx.request.body as Object;
+    const patchDividend = {
+      ...updateData,
+      updatedAt: new Date().toISOString(),
+    }
+    db.collection('dividends').doc(dividendsId).update(patchDividend);
+    ctx.body = {
+      status: 'ok',
+    }
+  } catch (error: any) {
+    console.error(error.stack);
+    ctx.throw(500, 'Failed to update dividend');
+  }
+})
+
+router.delete('/dividends/:id', async (ctx) => {
+  try {
+    console.log('delete dividend');
+    const dividendId = ctx.params.id;
+    db.collection('dividends').doc(dividendId).delete();
+    ctx.body = {
+      status: 'ok',
+    };
+  } catch (error: any) {
+    console.error(error.stack);
+    ctx.throw(500, 'Failed to delete dividend');
+  }
+})
+
 export default router;
